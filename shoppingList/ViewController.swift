b1da19ba89bf1fc94642669822752d09546bd319
JMallian <jlmallian@gmail.com>
2017-10-23 15:12:20 -0500

    working on title screen - I have a label

--- shoppingList/ViewController.swift
+++ shoppingList/ViewController.swift
@@ -9,11 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let appNameString = "Shopping List"
+    let appName: UILabel
+    let createNewButton: UIButton
+    let selectListButton: UIButton
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        appName = UILabel()
+        createNewButton = UIButton()
+        selectListButton = UIButton()
+        
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        let screenSize: CGSize = UIScreen.main.bounds.size
+        let centerX: CGFloat = screenSize.width/2
+        let centerY: CGFloat = screenSize.height/2
+        
+        appName.frame = CGRect(x: centerX - 100, y: centerY - 100, width: 200, height: 80)
+        appName.text = appNameString
+        appName.textAlignment = NSTextAlignment.center
+        appName.textColor = UIColor.cyan
+        appName.numberOfLines = 0
+
+        
+        self.view.addSubview(appName)
+    
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("I can't wait to push this to GitHub!")
     }
 
     override func didReceiveMemoryWarning() {
